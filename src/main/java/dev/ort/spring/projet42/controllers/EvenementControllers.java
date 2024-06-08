22ddb4ae95cebcc587c527435cebb24efeb2d347
jovansubasic <jovansubasic@gmail.com>
2024-06-08 10:41:27 +0200

    initialisation de keycloak et de security

--- src/main/java/dev/ort/spring/projet42/controllers/EvenementControllers.java
+++ src/main/java/dev/ort/spring/projet42/controllers/EvenementControllers.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/evenements")
+@RequestMapping("/api/evenements")
 public class EvenementControllers {
 
     @Autowired
--- src/main/java/dev/ort/spring/projet42/controllers/SportControllers.java
+++ src/main/java/dev/ort/spring/projet42/controllers/SportControllers.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/sports")
+@RequestMapping("/api/sports")
 public class SportControllers {
 
     @Autowired
